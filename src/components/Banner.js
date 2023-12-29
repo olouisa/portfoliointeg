68973676cc9ef69f51a9e07c3d347f6e4116ba24
Louisa Ongenyi <o.louisa@ymail.com>
2023-12-30 00:06:55 +0100

    shadows

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import "../styles/banner.css"
+import React, {useState, useEffect} from 'react';
+import "../styles/banner.css";
+import datas from "../lists/projects.json";
 
 function Banner() {
+  // const [currentIndex, setCurrentIndex] = useState(0);
+  // const pictures = datas.carrousel;
+
+  // useEffect(()=> {
+  //   const interval = setInterval(() => {
+  //   setCurrentIndex((prev) => (prev + 1)%pictures.length)
+  //   }, 3000);
+
+  //   return () => clearInterval(interval);
+  // }, [])
+  
+
   return (
-    <div className='banner-content' id='accueil' style={{backgroundImage: `url(${require(`../assets/images/coding.png`)})`, backgroundRepeat  : 'no-repeat', backgroundSize: 'cover'}}>
+    <div className='banner-content fade-transition' id='accueil'style={{backgroundImage: `url(${require('../assets/images/coding.png')})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}
+    >
         {/* <img className='banner-img' src={require(`../assets/images/coding.png`)} alt="" /> */}
         <div className='banner-filter'></div>
         <p className='banner-title'>Bonjour, je suis Louisa, développeuse web front-end junior.<br/> Bienvenue sur mon site portfolio.</p>
