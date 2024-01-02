e031265b204b9de5cb588c832b6732a580369986
Louisa Ongenyi <o.louisa@ymail.com>
2024-01-03 00:32:40 +0100

    icons hover2

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import "../styles/banner.css";
+// import { SplitText } from 'gsap/all';
+// import SplitText from 'gsap-trial/SplitText';
+// import { useGSAP } from "@gsap/react";
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import datas from "../lists/projects.json";
 
+// gsap.registerPlugin(SplitText);
+
 function Banner() {
+
   // const [currentIndex, setCurrentIndex] = useState(0);
   // const pictures = datas.carrousel;
 
@@ -16,20 +22,38 @@ function Banner() {
   //   return () => clearInterval(interval);
   // }, [])
 
+  // const spans1 = document.querySelectorAll('.dev-title span');
+  // let split1 = SplitText.create(".dev-title", {type:"words,chars"});
+
+  const spans1 = useRef([]);
+  const spans2 = document.querySelectorAll('.name span');
+  const navBtns = document.querySelectorAll('.navBtns');
+  const medias = document.querySelectorAll('icon');
+  const l1 = document.querySelector('l1');
+  const l2 = document.querySelector('l2');
+  console.log(spans1)
+
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      // const split1 = new SplitText(spans1.current, {type: 'chars'});
+      // const TL = gsap.timeline();
+
+      // TL.from(split1.chars,{ duration: 1, y: -50, opacity: 0, ease: 'power2.out'}, "+=0.3");
+  
+    });
+}, [])  
+
+
   const scrollToSites = () => {
-    // Utilisez le sélecteur d'ID pour accéder à l'élément "sites" et faites défiler la vue
     document.getElementById('sites').scrollIntoView({ behavior: 'smooth' });
   };
   const scrollToProjects = () => {
-    // Utilisez le sélecteur d'ID pour accéder à l'élément "sites" et faites défiler la vue
     document.getElementById('projets').scrollIntoView({ behavior: 'smooth' });
   };
   const scrollToStack = () => {
-    // Utilisez le sélecteur d'ID pour accéder à l'élément "sites" et faites défiler la vue
     document.getElementById('competences').scrollIntoView({ behavior: 'smooth' });
   };
   const scrollToContact = () => {
-    // Utilisez le sélecteur d'ID pour accéder à l'élément "sites" et faites défiler la vue
     document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
   };
 
@@ -38,29 +62,29 @@ function Banner() {
     >
       {/* <div className='banner-filter'></div> */}
       {/* <div className='textDiv'> */}
-        <div className='lines'>
-          <div className='l1'></div>
-          <div className='l2'></div>
+      <div className='lines'>
+        <div className='l1'></div>
+        <div className='l2'></div>
+      </div>
+
+      <div className='titles-container'>
+        <h1 className='dev-title banner-title' ref={spans1}>Développeuse Web Frontend Junior</h1>
+        <h2 className='name banner-title'><span>Louisa </span><span>Ongenyi</span></h2>
+        <div className='btns-container'>
+          <button className='navBtns b1' onClick={scrollToSites}>Sites en lignes</button>
+          <button className='navBtns b2' onClick={scrollToProjects}>Projets Web</button>
+          <button className='navBtns b3' onClick={scrollToStack} >Tech Stack</button>
+          <button className='navBtns b4' onClick={scrollToContact} >Contact</button>
+
+
         </div>
-
-        <div className='titles-container'>
-          <h1 className='dev-title banner-title'><span>Développeuse </span><span>Web </span><span>Frontend </span><span>Junior </span></h1>
-          <h2 className='name banner-title'><span>Louisa </span><span>Ongenyi</span></h2>
-          <div className='btns-container'>
-            <button className='navBtns b1' onClick={scrollToSites}>Sites en lignes</button>
-            <button className='navBtns b2' onClick={scrollToProjects}>Projets Web</button>
-            <button className='navBtns b3' onClick={scrollToStack} >Tech Stack</button>
-            <button className='navBtns b4'onClick={scrollToContact} >Contact</button>
+      </div>
 
 
-          </div>
-        </div>
-
-
-        <div className='medias-icons'>
-          <div className='icon'><a href='https://www.linkedin.com/in/
-louisa-ongenyi'><i class="fa-brands fa-linkedin" alt='Linkedin' target="_blank" rel="noopener noreferrer"></i></a></div>
-          <div className='icon'><a href='https://github.com/olouisa?tab=repositories' target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github" alt='GitHub'></i></a></div>
+      <div className='medias-icons'>
+        <div className='icon'><a href='https://www.linkedin.com/in/
+louisa-ongenyi'><i className="fa-brands fa-linkedin" alt='Linkedin' target="_blank" rel="noopener noreferrer"></i></a></div>
+        <div className='icon'><a href='https://github.com/olouisa?tab=repositories' target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github" alt='GitHub'></i></a></div>
 
         {/* </div> */}
       </div>
